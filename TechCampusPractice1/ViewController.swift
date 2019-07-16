a07b35f6ae0c3eb267dbce072cb17a770a6aa508
TechCampus <techcampus@razans-MacBook-Pro.local>
2019-07-16 23:12:42 +0300

    added image to launch screen, main storyboard with constraints

---
+++ TechCampusPractice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechCampusPractice1
+//
+//  Created by TechCampus on 7/16/19.
+//  Copyright © 2019 TechCampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
